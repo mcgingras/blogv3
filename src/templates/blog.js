54f9452115b97760cdbca7d91dcbb0ea4521676e
Michael Gingras <mcg79@cornell.edu>
2019-11-12 01:04:36 -0500

    adding css back hopefully

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,18 +1,22 @@
 import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
-import smile from '../images/smile.png';
+
+import "../style/index.scss";
+import "../style/font.scss";
 
 export default ({ data }) => {
   const post = data.markdownRemark
-  console.log(post);
+  var newDate = new Date(post.frontmatter.date.split(' ')[0]);
+  console.log(newDate);
+  
   return (
     <div>
       <SEO title="Home" />
       <div className="container">
         <div className="container--title small">
           <p>{post.frontmatter.title}</p>
-          <p>{post.frontmatter.date}</p>
+          <p>{newDate.toString()}</p>
         </div>
         <div className='container--title big'>
          <section className="content" dangerouslySetInnerHTML={{ __html: post.html }} />
