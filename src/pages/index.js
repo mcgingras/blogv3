f3a5381c0dc1ed75264e827b094cd1850574b485
Michael Gingras <mcg79@cornell.edu>
2019-11-19 21:53:46 -0500

    new blog againgit add .

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,16 +38,29 @@ const IndexPage = ({ data }) => {
     <div>
       <SEO title="Home" />
         <div className="container">
-          <div className="container--title small">
-            {/* <p>Michael's Studio</p> */}
-            <p className="logo--container">
-              {renderStringWithCOLOR("Michael's Studio")}
-            </p>
-            <p>Currently on a mini sabbatical doing small research projects in a great effort to figure out where my life needs to flow.</p>
+          <div>
+            <img src={smile} />
+            <h1>Hi, It's Michael. I'm exploring what it means for computers to be creative, and writing about the internet.</h1>
+            <div className="half">
+            <p>Right now I'm taking a mini sabatical to focus on research areas I find particularly interesting (currently, computers + creativity). Previously I've worked at Nasdaq R&D, <a href="https://ideocolab.com">IDEO CoLab,</a> and studied CS at Cornell.</p>
+            <p>Outside of work I'm active in the triathlon community, and will be representing <a href="https://fullsendtriathlon.com">Full Send</a> in the 2020 season.</p>
+            </div>
           </div>
-          <div className='container--img-faded big'>
+
+          <hr className="divider" />
+
+          <div>
             <PostList posts={posts} /> 
           </div>
+
+          <hr className="divider" />
+
+          <footer>
+            <a mailto="mcg79@cornell.edu">email</a>
+            <a href="https://are.na/michael-gingras-aqtwtixvoky">are.na</a>
+            <a href="https://twitter.com/michaelgingras">twitter</a>
+            <a href="https://github.com/mcgingras">github</a>
+          </footer>
         </div>
     </div>
   )
